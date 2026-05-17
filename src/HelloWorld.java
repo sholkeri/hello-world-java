a6cc211f3f4be3e7c0ff75f7998ff2e4b0c06562
sholkeri <sholkeri@gmail.com>
2026-05-17 13:46:15 +0300

    New feature branch and new method added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World_update!");
+        System.out.println(greeting("Sampo"));
+    }
+
+    static String greeting(String name) {
+        return "Hello, " + name + "!";
     }
 }
